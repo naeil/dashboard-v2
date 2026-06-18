c66e637fd43be8560f245c114468599510063762
seungwoo31 <seungwoo920301@gmail.com>
2026-06-18 12:28:25 +0900

    Create AiReviewAnalysisRepository.java

---
+++ src/main/java/naeil/dashboard/repository/AiReviewAnalysisRepository.java
@@ -0,0 +1,23 @@
+package naeil.dashboard.repository;
+
+import naeil.dashboard.entity.AiReviewAnalysis;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AiReviewAnalysisRepository extends JpaRepository<AiReviewAnalysis, Long> {
+
+    Optional<AiReviewAnalysis> findByReviewId(Long reviewId);
+
+    List<AiReviewAnalysis> findByIsUrgentTrue();
+
+    List<AiReviewAnalysis> findByReplyStatus(String replyStatus);
+
+    List<AiReviewAnalysis> findBySentiment(String sentiment);
+
+    long countByIsUrgentTrue();
+
+    long countBySentiment(String sentiment);
+}
