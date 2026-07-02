ea92cd136b1d6857889b071914ebd3b2fed0c230
seungwoo31 <seungwoo920301@gmail.com>
2026-07-02 17:21:49 +0900

    Update EventProps.java

--- highfree-event/backend/src/main/java/kr/co/highfree/event/config/EventProps.java
+++ highfree-event/backend/src/main/java/kr/co/highfree/event/config/EventProps.java
@@ -1,14 +1,10 @@
 package kr.co.highfree.event.config;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Getter
-@Setter
 @Component
 @ConfigurationProperties(prefix = "app")
 public class EventProps {
@@ -17,12 +13,31 @@ public class EventProps {
     private String corsOrigins;
     private List<RewardConfig> rewards;
 
-    @Getter
-    @Setter
+    public String getAdminKey() { return adminKey; }
+    public void setAdminKey(String adminKey) { this.adminKey = adminKey; }
+
+    public String getCorsOrigins() { return corsOrigins; }
+    public void setCorsOrigins(String corsOrigins) { this.corsOrigins = corsOrigins; }
+
+    public List<RewardConfig> getRewards() { return rewards; }
+    public void setRewards(List<RewardConfig> rewards) { this.rewards = rewards; }
+
     public static class RewardConfig {
         private String key;
         private String label;
         private int points;
         private int weight;
+
+        public String getKey() { return key; }
+        public void setKey(String key) { this.key = key; }
+
+        public String getLabel() { return label; }
+        public void setLabel(String label) { this.label = label; }
+
+        public int getPoints() { return points; }
+        public void setPoints(int points) { this.points = points; }
+
+        public int getWeight() { return weight; }
+        public void setWeight(int weight) { this.weight = weight; }
     }
 }
