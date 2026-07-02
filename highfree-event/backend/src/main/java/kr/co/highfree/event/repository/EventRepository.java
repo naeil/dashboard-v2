59faa1dc41b8a1a7cad16079fcdb7919b2155765
seungwoo31 <seungwoo920301@gmail.com>
2026-07-02 17:33:39 +0900

    Update EventRepository.java

--- highfree-event/backend/src/main/java/kr/co/highfree/event/repository/EventRepository.java
+++ highfree-event/backend/src/main/java/kr/co/highfree/event/repository/EventRepository.java
@@ -1,18 +1,2 @@
 package kr.co.highfree.event.repository;
-
-import kr.co.highfree.event.domain.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-@Repository
-public interface QrCodeRepository extends JpaRepository<QrCode, Long> {
-    Optional<QrCode> findByQrId(String qrId);
-}
-
+// Repositories defined in Repositories.java
