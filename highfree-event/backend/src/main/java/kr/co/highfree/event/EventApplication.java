41950c9c0afb7c4e07369fb66d7703c96925b650
seungwoo31 <seungwoo920301@gmail.com>
2026-07-02 16:14:38 +0900

    feat: add highfree-event project - EventApplication

---
+++ highfree-event/backend/src/main/java/kr/co/highfree/event/EventApplication.java
@@ -0,0 +1,11 @@
+package kr.co.highfree.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+  public class EventApplication {
+        public static void main(String[] args) {
+                  SpringApplication.run(EventApplication.class, args);
+        }
+  }
