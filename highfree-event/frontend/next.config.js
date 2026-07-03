14412ed36521c7f0a9f0f138cea0de7b86f2584a
seungwoo31 <seungwoo920301@gmail.com>
2026-07-03 09:01:28 +0900

    Update next.config.js

--- highfree-event/frontend/next.config.js
+++ highfree-event/frontend/next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://highfree-event-api.onrender.com',
+  },
 }
 
 module.exports = nextConfig
